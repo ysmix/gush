773dddb279eaa974ddea47cba32da19724328e68
ysmix <34684116+ysmix@users.noreply.github.xom>
2018-05-13 11:31:37 +0900

    APIのレスポンスを表示するように修正

--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="event-list">
     <h1>EventList</h1>
+    <ul>
+      <li v-for="event in event_list" :key="event.id">
+        <a :href="'https://connpass.com/event/' + event.id + '/'">{{event.title}}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
+export default {
+  name: 'event_list',
+  data () {
+    return {
+      event_list: []
+    }
+  },
+  async created () {
+    try {
+      let response = await axios.get('http://localhost:8080/event?keyword=Lambda')
+      this.event_list = response.data
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
 </script>
 
 <style scoped>
